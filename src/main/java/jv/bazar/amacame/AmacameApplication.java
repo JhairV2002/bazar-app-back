ef3e0e6c4d0c7a6f228cc1284e7d9107b8c1ec47
JhairV2002 <jhairvasquez2002@gmail.com>
2024-01-27 01:23:07 -0500

    first commit

---
+++ src/main/java/jv/bazar/amacame/AmacameApplication.java
@@ -0,0 +1,13 @@
+package jv.bazar.amacame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmacameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmacameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jv/bazar/amacame/AmacameApplicationTests.java
@@ -0,0 +1,13 @@
+package jv.bazar.amacame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmacameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
